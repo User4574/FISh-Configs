03989bd7448998053242fc63d9d9fbf286195284
Nathan Lasseter <nathan.je.lasseter@googlemail.com>
2012-08-12 12:22:02 +0100

    Alias update

--- config.fish
+++ config.fish
@@ -2,8 +2,8 @@ alias she='ssh njel500@milan.cs.york.ac.uk'
 alias sft='sftp njel500@milan.cs.york.ac.uk'
 alias shx='ssh -YC njel500@milan.cs.york.ac.uk'
 alias 4574='ssh nathan@4574.co.uk'
-alias 4574r='ssh nathan@4574.co.uk -R 2222:localhost:2222'
-alias page='sshfs nathan@4574.co.uk:p/ ~/mnt'
 alias umnt='fusermount -u ~/mnt'
+alias grope='ping'
 
-set -x PATH /home/nathan/bin $PATH /opt/android-sdk-linux_x86/platform-tools/
+set -x PATH /home/nathan/bin $PATH /opt/android-sdk-linux_x86/platform-tools/# /opt/moterunner/linux/bin/
+#set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH /opt/moterunner/linux/bin/
