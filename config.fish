dcfd4df060a3286cd563a67ac0384c703285d700
Nathan Lasseter <nathan@4574.co.uk>
2012-12-12 21:29:52 +0000

    New Prompt!

--- config.fish
+++ config.fish
@@ -2,8 +2,5 @@ alias she='ssh njel500@milan.cs.york.ac.uk'
 alias sft='sftp njel500@milan.cs.york.ac.uk'
 alias shx='ssh -YC njel500@milan.cs.york.ac.uk'
 alias 4574='ssh nathan@4574.co.uk'
-alias 4574r='ssh nathan@4574.co.uk -R 2222:localhost:2222'
-alias page='sshfs nathan@4574.co.uk:p/ ~/mnt'
-alias umnt='fusermount -u ~/mnt'
 
 set -x PATH /home/nathan/bin $PATH /opt/android-sdk-linux_x86/platform-tools/
