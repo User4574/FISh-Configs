e7b48b0663b3879e1d31aa604652c635493b5ca4
Nathan Lasseter <nathan.je.lasseter@googlemail.com>
2012-12-13 19:47:10 +0000

    config.fish merge

--- config.fish
+++ config.fish
@@ -1,9 +1,8 @@
 alias she='ssh njel500@milan.cs.york.ac.uk'
 alias sft='sftp njel500@milan.cs.york.ac.uk'
 alias shx='ssh -YC njel500@milan.cs.york.ac.uk'
-alias 4574='ssh nathan@4574.co.uk'
-alias umnt='fusermount -u ~/mnt'
-alias grope='ping'
+alias 4574='ssh 4574'
+alias pcorr='chmod -R a+rX,u+w .'
 
-set -x PATH /home/nathan/bin $PATH /opt/android-sdk-linux_x86/platform-tools/# /opt/moterunner/linux/bin/
-#set -x LD_LIBRARY_PATH $LD_LIBRARY_PATH /opt/moterunner/linux/bin/
+set -x PATH /home/nathan/bin $PATH /opt/android-sdk-linux_x86/platform-tools/
+set -x EDITOR vim
