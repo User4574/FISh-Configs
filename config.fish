5f67b5bb58663b057b7f4355d3e131da9f7eec4a
Nathan Lasseter <Nathan Lasseter nathan@bytemark.co.uk>
2013-07-25 09:48:32 +0100

    removed cs stuff and imgup and steam

--- config.fish
+++ config.fish
@@ -1,11 +1,6 @@
-alias she='ssh njel500@milan.cs.york.ac.uk'
-alias sft='sftp njel500@milan.cs.york.ac.uk'
-alias shx='ssh -YC njel500@milan.cs.york.ac.uk'
-
 alias what='ps -ef'
 alias when='uptime'
 
-set -x PATH /home/nathan/bin $PATH /opt/adk/platform-tools
+set -x PATH /home/nathan/bin $PATH
 set -x EDITOR vim
 set -x CDPATH '.'
-set -x STEAM_FRAME_FORCE_CLOSE 1
--- functions/imgup.fish
+++
@@ -1,7 +0,0 @@
-function imgup
-	if test -n (echo $argv | awk '{print $2}')
-		cat $argv[1] | ssh nathan@4574.co.uk "sudo su - www-data -c 'cat > /var/www/gallery/$argv[2]'"
-	else
-		cat $argv[1] | ssh nathan@4574.co.uk "sudo su - www-data -c 'cat > /var/www/gallery/$argv[1]'"
-	end
-end
